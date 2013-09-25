071dcc398d901c53668e70608b2afb98490d6462
Steven Brooks <brooks.stevena@gmail.com>
2013-09-25 14:32:38 -0400

    made store with spreed (rails 4.0.0 :( )

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SpreeStore::Application.config.secret_key_base = '41ebb0f619f2c94637bd565306749d8895513ff63b5d1441a1951d949835d5988d525403f0c5233ab8034eeea3d1add5e8167a207c57fec3295ceb4ac160010b'
